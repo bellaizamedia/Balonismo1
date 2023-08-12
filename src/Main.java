fbc845acfc76ff6b7f9f6241ce5d915d3b888a66
Izabella Oliveira <contato.bellaiza@gmail.com>
2023-08-11 22:57:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,52 @@
 public class Main {
     public static void main(String[] args){
-        //criar objetos do passeio
-        Pessoa p1 = new Pessoa();
-        Pessoa p2; //DECLARAÇÃO DO OBJETO
-        p2 = new Pessoa();
-        //...
-        p1.nome = "Elliot Page";
-        p1.cpf = "12345678990";
-        p1.rg = "1122222154465";
-        p1.telefone = 988888888;
-        p1.numeroParaEmergencia = 977777777;
-        p1.endereço = "rua juliana paiva, número 44";
-        p1.termoCienciaRisco = true;
+      import java.util.Scanner;
 
-        p2.nome = "Lana del Rey";
-        p2.cpf = "12345678997";
-        p2.rg = "11222221544665";
-        p2.telefone = 988855888;
-        p2.numeroParaEmergencia = 977227777;
-        p2.endereço = "rua juliana paiva, número 44";
-        p2.termoCienciaRisco = true;
+import Piloto;
+import Balao;
+import Pessoa;
 
-        Piloto pl1 = new Piloto();
-        pl1.nome = "Calleri";
-        pl1.cpf = "15555555555";
-        pl1.NumeroRegistro = "8645555";
+public class Main {
+    public static void main(String[] args) {
+ Scanner tcd = new Scanner(System.in);
+ int x =0;
+        while  (x == 0){
 
-        Balao b1 = new Balao();
-        b1.NumeroIdentificacao = "861445";
-        b1.RegistroRegularidade = "regular";
+            System.out.println("Bem vindo a agencia de balão, digite 1 se quiser adicionar um balão, digite 2 se quiser ver o balão já registrado, digite 3 se quiser registrar um piloto, digite 4 se quiser ver o piloto já regisrado, digite 5 se quiser registrar um passageiro, digite 6 se quiser ver o passsageiro já registrado");
 
+             x = tcd.nextInt();
 
+            if (x == 1){
+                 Balao b1 = new Balao();
+                 b1.registroBalao();
+            }
+            else if(x == 2){
+                Balao b2 = new Balao();
+                b2.registradoBalao();
+            }
+            else if (x == 3){
+                Piloto p1 = new Piloto();
+                p1.registroPiloto();
+            }
+            else if (x == 4){
+                Piloto p2 = new Piloto();
+                p2.registradoPiloto();
+            }
+            else if (x == 5){
+                Pessoa pe1 = new Pessoa();
+                pe1.registroPessoa();
+            }
+            else if (x == 6){
+                Pessoa pe2 = new Pessoa();
+                pe2.registradoPessoa();
+            }
 
-    }
+            System.out.println("Deseja continuar no Sistema? " + "0 para sim / 1 para não ");
+             x = tcd.nextInt();
+        }
+        
+
+   
+}
+
 }
